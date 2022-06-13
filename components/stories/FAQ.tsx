20d1591f37837456e09a3221d2a880a2636a5c9a
Antonis Valamontes <tony@geopay.me>
2022-06-13 11:51:22 +0300

    Update FAQ.tsx

--- components/stories/FAQ.tsx
+++ components/stories/FAQ.tsx
@@ -9,10 +9,10 @@ const FAQ: FunctionComponent = () => {
       subtitle="Here are some of the most asked questions our team has received during the process of making this project, check out our responses below!"
       items={[
         {
-          title: 'Why do I need a monster?',
+          title: 'Why do I need a Cultivar?',
           content: (
             <>
-              Monster Shelter is more than an art project, it is a game and discord bot! The monsters will be tied to a p2e game with elements of the RPG and Tamagotchi, which is in its early stages of development. The economic part will be built around the $TROPHY usability token, and the first stage with the farming part will become available after the main sale. Later you will be able to feed, care for and improve your monster. We also want to <a href="#">make a bot</a> for the convenience of our community members there. Discord bot will repeat the browser game mechanics and will be released with a slight delay from the browser game. This will be the start of a long journey for Monster Shelter.
+              The Cali Tribe Cultivars is more than an art project, it is a game on the The monsters will be tied to a p2e game with elements of the RPG and Tamagotchi, which is in its early stages of development. The economic part will be built around the $TROPHY usability token, and the first stage with the farming part will become available after the main sale. Later you will be able to feed, care for and improve your monster. We also want to <a href="#">make a bot</a> for the convenience of our community members there. Discord bot will repeat the browser game mechanics and will be released with a slight delay from the browser game. This will be the start of a long journey for Monster Shelter.
             </>
           )
         },
@@ -21,15 +21,15 @@ const FAQ: FunctionComponent = () => {
           content: 'Of course! Monsters will be distributed according to the ERC-721 standard. You will be able to trade them on platforms like OpenSea.'
         },
         {
-          title: 'What is a Tamagotchi game and discord bot?',
-          content: 'Monster Shelter is a p2e game with Tamagotchi elements. The NFT you minted is tied to the game and becomes your pet. By sending a monster to the Hunt, you will earn $TROPHY utility tokens, which in the future you can exchange and spend in the in-game store for useful items and rare NFT. Parallel to this, you will need to take care of the pet, feed it, raise its level, send it to wild duels and climb the leaderboard. A breeding system will also be introduced in one of the future updates. The bot will be a copy of the browser game for Discord server.'
+          title: 'What is The Street Metaverse Game?',
+          content: 'The Street Metaverse Game is a unity game with blockchain elements. The NFT you minted is tied to the game and becomes your pet. By sending a monster to the Hunt, you will earn $TROPHY utility tokens, which in the future you can exchange and spend in the in-game store for useful items and rare NFT. Parallel to this, you will need to take care of the pet, feed it, raise its level, send it to wild duels and climb the leaderboard. A breeding system will also be introduced in one of the future updates. The bot will be a copy of the browser game for Discord server.'
         },
         {
-          title: 'Will there be P2E?',
-          content: 'Yes! The economic basis of the game - $TROPHY utility tokens. All monsters holders will be able to receive it. To do this, you will need to send the monster to the Hunt, where he will mine $TROPHY. Hunting is the main mechanic of earning tokens and includes an element of randomness. At the first stage of the game, holders will have the opportunity to collect tokens for the further game. Later they will be able to use them to exchange or purchase various useful items and rare NFT prizes in the in-game store. Who started earlier will be in a better position in the future, you can already accumulate start-up capital while waiting for updates. Beyond the fundamental P2E mechanics, the goal of our development is to create a comfortable environment for you to have a good time, make new friends and get passive profit. This game will delight Tamagotchi fans!'
+          title: 'What is a REAL Token',
+          content: 'Yes! The economic basis of the game - REAL utility tokens. The Street Metaverse Alter Natives  will be able to receive it. To do this, you will need to send the monster to the Hunt, where he will mine $TROPHY. Hunting is the main mechanic of earning tokens and includes an element of randomness. At the first stage of the game, holders will have the opportunity to collect tokens for the further game. Later they will be able to use them to exchange or purchase various useful items and rare NFT prizes in the in-game store. Who started earlier will be in a better position in the future, you can already accumulate start-up capital while waiting for updates. Beyond the fundamental P2E mechanics, the goal of our development is to create a comfortable environment for you to have a good time, make new friends and get passive profit. This game will delight Tamagotchi fans!'
         },
         {
-          title: 'When is the game release?',
+          title: 'When is the Game release?',
           content: 'The game itself is in an early stage of development. After the main sale, the first stage of the game will open. You will have the opportunity to send a monster on a hunt to farm tokens in several different locations. With updates, we will add new features: wild duels, in-game store, breeding and much more! We want to provide you with a quality and profitable product, so we don’t want to rush and plan a long and confident development together with the community. Main game mechanics will be introduced in the first half of 2022.'
         },
         {
